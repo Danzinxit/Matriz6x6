6c0e393b07561198c7a2bc94ca8f754ec0f495d5
Daniel Vieira <124292470+Danzinxit@users.noreply.github.com>
2025-03-06 20:19:24 -0300

    Add files via upload

---
+++ AulaMatriz/AulaMatriz/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                int[,] numeros = {
+                { 2, 0, 0, 0, 0, 0 },
+                { 1, 2, 0, 0, 0, 0 },
+                { 1, 1, 2, 0, 0, 0 },
+                { 1, 1, 1, 2, 0, 0 },
+                { 1, 1, 1, 1, 2, 0 },
+                { 1, 1, 1, 1, 1, 2 }
+            };
+
+            for (int i = 0; i < numeros.GetLength(0); i++)
+            {
+                for (int j = 0; j < numeros.GetLength(1); j++)
+                {
+                    Console.Write(numeros[i, j] + "\t"); 
+                }
+                Console.WriteLine(); 
+            }
+        }
+            */
+
+
+            int[,] mat = new int[6, 6];
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+                
+
+            {
+              
+                for (int j = 0; j < mat.GetLength(1); j++)
+                    
+                {
+
+                    mat[i, j] = 1;
+                    mat[j, i] = 0;
+                  
+                }
+                mat[i, i] = 2;
+
+            }
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    Console.Write(mat[i, j]  + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
